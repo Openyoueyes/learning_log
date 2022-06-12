81bdf535d2f2bb1c1e3a79b9d97ced975561f5ad
Openyoueyes <iliaglo93@gmail.com>
2022-06-13 00:26:26 +0300

    add read/add new topic and entry

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 #"""Определяет схемы URL для learning_logs."""
 from learning_logs import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
     url(r'', include(('learning_logs.urls','learning_logs'), namespace='learning_logs')),
 # Домашняя страница
     url('', views.index, name='index'),
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -14,8 +14,8 @@ class Entry(models.Model):
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-class Meta:
-    verbose_name_plural = 'entries'
+    class Meta:
+        verbose_name_plural = 'entries'
     def __str__(self):
         if len(self.text) > 50:
 #"""Возвращает строковое представление модели."""
